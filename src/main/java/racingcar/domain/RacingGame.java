4670813959a3d4866a184e98a968ffd98388a5ad
jongyo0N <jongyoon5960@kookmin.ac.kr>
2025-10-27 17:09:18 +0900

    feat(game): RacingGame 실행 로직 구현
    시도 횟수만큼 라운드를 반복하며 각 자동차의 이동 여부를 MoveStrategy로 판단하고 Cars 컬렉션을 업데이트
    라운드 종료 후 ResultView를 통해 각 자동차의 상태를 출력하고, 최종 우승자를 안내

---
+++ src/main/java/racingcar/domain/RacingGame.java
@@ -0,0 +1,37 @@
+package racingcar.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RacingGame {
+
+    private final Cars cars;
+    private final MoveStrategy moveStrategy;
+    private final ResultView resultView;
+
+    public RacingGame(Cars cars, MoveStrategy moveStrategy, ResultView resultView) {
+        this.cars = cars;
+        this.moveStrategy = moveStrategy;
+        this.resultView = resultView;
+    }
+
+    /**
+     * 주어진 시도 횟수만큼 게임을 진행
+     */
+
+    public void run(int attempts) {
+        for (int i = 0; i < attempts; i++) {
+            performRound();
+            resultView.printRound(cars.getCars());
+        }
+        resultView.printWinners(cars.winners());
+    }
+
+    private void performRound() {
+        List<Boolean> moves = new ArrayList<>();
+        for (var car : cars.getCars()) {
+            moves.add(moveStrategy.shouldMove());
+        }
+        cars.moveAll(moves);
+    }
+}
